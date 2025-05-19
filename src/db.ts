f8693c841674cf6d60378184ac066c2754645629
kcaparas1630 <kcaparas1630@gmail.com>
2025-05-19 15:12:43 -0700

    get user for login method

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { FirebaseDatabaseError } from "firebase-admin/lib/utils/error";
+import * as admin from 'firebase-admin';
 
 const prisma = new PrismaClient({
     log: ['query', 'info', 'warn', 'error'],
@@ -13,4 +14,36 @@ prisma.$connect().then(() => {
 
 export class InterviewService {
 // TODO: Implement the static methods for the interview service
+// get user from firebase token
+    static async getUserFromFirebaseToken(uid: string) {
+        try {
+            // fetch user from database using prisma
+            const user = await prisma.user.findUnique({
+                where: {
+                    firebaseUid: uid,
+                }
+            });
+            // create user if not found
+            if (!user) {
+                const firebaseUser = await admin.auth().getUser(uid);
+                const newUser = await prisma.user.create({
+                    data: {
+                        firebaseUid: uid,
+                        profile: {
+                            name: firebaseUser.displayName ?? "",
+                            email: firebaseUser.email ?? "",
+                            lastLogin: new Date(),
+                        },
+                        createdAt: new Date(),
+                        updatedAt: new Date(),
+                    }
+                });
+                return newUser;
+            }
+            return user;
+        } catch (error) {
+            console.error('Error fetching user from firebase token:', error);
+            throw new Error('Failed to fetch user from firebase token');
+        }
+    }
 }
