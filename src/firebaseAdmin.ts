41796dace47d2dcbef4983b51835ad3bb4bca0fc
kcaparas1630 <kcaparas1630@gmail.com>
2025-05-22 10:31:31 -0700

    try if firebase will show in cloudrun

--- src/firebaseAdmin.ts
+++ src/firebaseAdmin.ts
@@ -3,9 +3,11 @@ import * as fs from 'fs';
 import path from 'path';
 import dotenv from 'dotenv';
 import { ServiceAccount } from 'firebase-admin';
+import logger from './Config/LoggerConfig';
 
 dotenv.config();
 
+logger.error(process.env.FIREBASE_PRIVATE_KEY);
 const keyPath = path.join(__dirname, 'Config', 'firebase-admin-key.json');
 let serviceAccount;
 // for local development
