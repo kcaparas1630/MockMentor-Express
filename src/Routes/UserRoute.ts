072d4ed9fb57caf737aca284fecbc6e99efbfac7
Kent Caparas <kcaparas1630@gmail.com>
2025-05-25 22:42:06 -0700

    added a patch method to router

--- src/Routes/UserRoute.ts
+++ src/Routes/UserRoute.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { createUser, getUser } from '../Controller/UserController';
+import { createUser, getUser, updateUser } from '../Controller/UserController';
 import verifyFirebaseToken from '../Middleware/VerifyFirebaseToken';
 const router = Router();
 
 router.get('/user', verifyFirebaseToken, getUser);
 router.post('/create-user', verifyFirebaseToken, createUser);
+router.put('/update-user', verifyFirebaseToken, updateUser);
 
 export default router;
