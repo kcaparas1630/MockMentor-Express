dbce62dd5e49ff031c65eb13efd3418db341f027
kcaparas1630 <kcaparas1630@gmail.com>
2025-05-29 18:14:09 -0700

    removed optional for params

--- src/Types/UserProfile.ts
+++ src/Types/UserProfile.ts
@@ -1,10 +1,10 @@
 import { UserRecord } from "firebase-admin/lib/auth/user-record";
 
 export interface ProfileData extends Partial<UserRecord> {
-    name?: string;
-    email?: string;
-    jobRole?: string;
-    lastLogin?: Date;
+    name: string;
+    email: string;
+    jobRole: string;
+    lastLogin: Date;
     password?: string; // for admin auth
 }
 
