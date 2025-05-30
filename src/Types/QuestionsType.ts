f2ea5fa5e555474297ed0b4bf8819cdb3cf7bec1
kcaparas1630 <kcaparas1630@gmail.com>
2025-05-30 11:24:57 -0700

    created a question feedback interface

--- src/Types/QuestionsType.ts
+++ src/Types/QuestionsType.ts
@@ -2,3 +2,11 @@ export interface Question {
     question: string;
     questionType: string;
 }
+
+export interface QuestionFeedback {
+    score: number;
+    feedback: string;
+    strengths: string[];
+    improvements: string[];
+    tips: string[];
+}
