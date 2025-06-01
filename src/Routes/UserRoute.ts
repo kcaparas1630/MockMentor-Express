4b9c0d2d4017332dd14bc28bc3272f48a2e8a754
Kent Caparas <kcaparas1630@gmail.com>
2025-05-31 20:58:45 -0700

    matching the other controller's function names

--- src/Routes/UserRoute.ts
+++ src/Routes/UserRoute.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import { createUserController, getUser, updateUserController } from '../Controller/UserController';
+import { createUserController, getUserController, updateUserController } from '../Controller/UserController';
 import verifyFirebaseToken from '../Middleware/VerifyFirebaseToken';
 const router = Router();
 
-router.get('/user', verifyFirebaseToken, getUser);
+router.get('/user', verifyFirebaseToken, getUserController);
 router.post('/create-user', createUserController);
 router.put('/update-user', verifyFirebaseToken, updateUserController);
 
